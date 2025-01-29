dff7329e7a61e545a0e9836b6fee47fd733eeebd
sumaaanth <sumanththota7@gmail.com>
2025-01-29 00:44:49 -0500

    minor text fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: false,
+  // images: {
+  //   unoptimized: true,
+  // },
 
   images: {
-    unoptimized: true,
+    loader: "akamai",
+    path: "",
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      ...defaultPathMap,
+      // Add custom paths here if needed
+    };
   },
 };
 
